2681f89384914b13e949f6f9df63ba3116d586ba
Adam <ada.avenger@gmail.com>
2024-01-31 16:34:04 +0100

    Template for NextJs projects

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+// Styles
+import styles from "./page.module.scss";
+// Public & Assets
+
+// React/Next Functions
+
+// Context
+
+// Componenets
+
+
+export default function Home() {
+  return (
+    <main>
+      X
+    </main>
+  );
+}
